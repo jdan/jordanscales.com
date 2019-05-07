41ff64c37434141df4e5ea5a19d4259e7f99d1c7
Jordan Scales <scalesjordan@gmail.com>
2019-05-07 19:59:01 -0400

    let's roll

---
+++ README.md
@@ -0,0 +1,9 @@
+# My rad personal internet webpage
+
+I installed Windows XP this weekend to play SimGolf and remembered how great the loading screen was.
+
+So I copied it!
+
+[http://jordanscales.com](http://jordanscales.com)
+
+[Remix on glitch](https://glitch.com/edit/#!/windows-xp)
---
+++ script.js
@@ -0,0 +1,38 @@
+var progressLeft = -24;
+var $progress = document.querySelector(".progress");
+var $main = document.querySelector("main");
+var $note = document.querySelector(".loading-note");
+
+var opacity = 0;
+var noteOpacity = 0;
+
+window.addEventListener("DOMContentLoaded", function () {
+    setInterval(function () {
+        progressLeft += 8;
+        if (progressLeft > 120) {
+            progressLeft = -24;
+        }
+
+        $progress.style.left = progressLeft + "px";
+    }, 100);
+
+    var interval = setInterval(function () {
+        opacity += 0.07;
+        if (opacity >= 1.0) {
+            clearInterval(interval);
+        }
+
+        $main.style.opacity = opacity;
+    }, 100);
+
+    setTimeout(function () {
+        var interval = setInterval(function () {
+            noteOpacity += 0.07;
+            if (noteOpacity >= 0.9) {
+                clearInterval(interval);
+            }
+
+            $note.style.opacity = noteOpacity;
+        }, 100);
+    }, 10000);
+});
