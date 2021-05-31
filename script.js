1f17e178b89c12b3540cb82b062a07de1c8724b0
Jordan Scales <scalesjordan@gmail.com>
2021-05-31 10:47:48 -0400

    prefers-reduced-motion

--- script.js
+++ script.js
@@ -7,32 +7,40 @@ var opacity = 0;
 var noteOpacity = 0;
 
 window.addEventListener("DOMContentLoaded", function () {
-    setInterval(function () {
-        progressLeft += 8;
-        if (progressLeft > 120) {
-            progressLeft = -24;
-        }
+  const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
+  if (mediaQuery.matches) {
+    $main.style.opacity = 1.0;
+    $note.style.opacity = 1.0;
+    $progress.style.left = "0px";
+    return;
+  }
 
-        $progress.style.left = progressLeft + "px";
-    }, 100);
+  setInterval(function () {
+    progressLeft += 8;
+    if (progressLeft > 120) {
+      progressLeft = -24;
+    }
 
+    $progress.style.left = progressLeft + "px";
+  }, 100);
+
+  var interval = setInterval(function () {
+    opacity += 0.07;
+    if (opacity >= 1.0) {
+      clearInterval(interval);
+    }
+
+    $main.style.opacity = opacity;
+  }, 100);
+
+  setTimeout(function () {
     var interval = setInterval(function () {
-        opacity += 0.07;
-        if (opacity >= 1.0) {
-            clearInterval(interval);
-        }
+      noteOpacity += 0.07;
+      if (noteOpacity >= 0.9) {
+        clearInterval(interval);
+      }
 
-        $main.style.opacity = opacity;
+      $note.style.opacity = noteOpacity;
     }, 100);
-
-    setTimeout(function () {
-        var interval = setInterval(function () {
-            noteOpacity += 0.07;
-            if (noteOpacity >= 0.9) {
-                clearInterval(interval);
-            }
-
-            $note.style.opacity = noteOpacity;
-        }, 100);
-    }, 10000);
+  }, 10000);
 });
